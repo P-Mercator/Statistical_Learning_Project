260b09f686fe311e5abefb44dc32e6a34fb6fede
Pablo Bordons Estrada <pablobordons@users.noreply.github.com>
2017-01-30 02:05:52 +0100

    all classified method ~58% + post analysis

--- scripts/logic_regression_one_vs_all.r
+++ scripts/logic_regression_one_vs_all.r
@@ -65,7 +65,7 @@ test_idx=(1:nrow(foo))[!(1:nrow(foo))%in%train_idx]
 ###################################################
 
 
-train_class <- function(y, Y) { # y is the name of the columns, Y is the label in test
+train_class <- function(y) { # y is the name of the columns, Y is the label in test
   
   f <- substitute(glm(y~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8 + V9 + V10
                          + V11 + V12 + V13 + V14 + V15 + V16 + V17 + V18 + V19 + V20
@@ -79,43 +79,71 @@ train_class <- function(y, Y) { # y is the name of the columns, Y is the label i
                             newdata=subset(foo[test_idx]),
                             type='response')
   
-  fitted.results <- ifelse(fitted.results > 0.45,1,0)
+  return (fitted.results)
   
-  misClasificError <- mean(fitted.results != Y)
+  #fitted.results <- ifelse(fitted.results > 0.45,1,0)
   
-  print(paste('Accuracy',1-misClasificError))
+  #misClasificError <- mean(fitted.results != Y)
   
-  return (fitted.results) # returns the prediction for y vs all
+  #print(paste('Accuracy',1-misClasificError))
+  
+  #return (fitted.results) # returns the prediction for y vs all
 }
 
 
-results.A = train_class(A, foo[test_idx]$A)
-results.B = train_class(B, foo[test_idx]$B)
-results.C = train_class(C, foo[test_idx]$C)
-results.Point = train_class(Point, foo[test_idx]$Point)
-results.V = train_class(V, foo[test_idx]$V)
-
-print(table(foo$label))
+results.A = train_class(A)
+results.B = train_class(B)
+results.C = train_class(C)
+results.Point = train_class(Point)
+results.V = train_class(V)
 
 
-results.label = 0
-results.label[results.Point == 1] = "Point"
-results.label[results.A == 1] = "A"
-results.label[results.B == 1] = "B"
-results.label[results.C == 1] = "C"
-results.label[results.V == 1] = "V"
+DF <- data.frame(A=results.A,
+                 B=results.B,
+                 C=results.C,
+                 V=results.V,
+                 Point=results.Point)
 
+results.label = colnames(DF)[apply(DF,1,which.max)]
+results.max_value = apply(DF,1,max)
 
-print(table(results.label, exclude = NULL))
+DF$label = results.label
+DF$max_value = results.max_value
 
-# Right now there are a lot of NA (instances that have not been classified)
-# I can substitute them by the most common label... but...
-# Point is also the one with less accuracy when performing individual classification 1 vs All
-
-results.label[is.na(results.label)] = "Point"
 
 misClasificError <- mean(results.label != foo[test_idx]$label)
 
 print(paste('Accuracy',1-misClasificError))
 
+table(results.label)
 
+mean(DF$Point[DF$label == "Point"])
+mean(DF$A[DF$label == "A"])
+mean(DF$B[DF$label == "B"])
+mean(DF$C[DF$label == "C"])
+mean(DF$V[DF$label == "V"])
+
+hist(DF$Point[DF$label == "Point"])
+hist(DF$A[DF$label == "A"])
+hist(DF$B[DF$label == "B"])
+hist(DF$C[DF$label == "C"])
+hist(DF$V[DF$label == "V"])
+
+hist(DF$max_value)
+
+DF$real = foo[test_idx]$label
+DF$success = (DF$label == DF$real)*1
+
+results.wrongs = DF$label[DF$label != DF$real]
+table(results.wrongs)
+
+wrongs.A = DF$real[DF$success == 0 & DF$label == "A"]
+table(wrongs.A)
+wrongs.B = DF$real[DF$success == 0 & DF$label == "B"]
+table(wrongs.B)
+wrongs.C = DF$real[DF$success == 0 & DF$label == "C"]
+table(wrongs.C)
+wrongs.V = DF$real[DF$success == 0 & DF$label == "V"]
+table(wrongs.V)
+wrongs.Point = DF$real[DF$success == 0 & DF$label == "Point"]
+table(wrongs.Point)
